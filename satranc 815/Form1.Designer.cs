0728abbe5095c18d511232fc824e33d5333c7f66
Yezda <145451081+yezdanuracik@users.noreply.github.com>
2024-01-15 10:18:30 +0300

    Add files via upload

---
+++ satranc 815/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace satranc_815
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnSiyah = new System.Windows.Forms.Button();
+            this.tmrBeyaz = new System.Windows.Forms.Timer(this.components);
+            this.tmrSiyah = new System.Windows.Forms.Timer(this.components);
+            this.btnBeyaz = new System.Windows.Forms.Button();
+            this.lblBeyaz = new System.Windows.Forms.Label();
+            this.lblSiyah = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(12, 76);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(108, 73);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnSiyah
+            // 
+            this.btnSiyah.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSiyah.Location = new System.Drawing.Point(181, 108);
+            this.btnSiyah.Name = "btnSiyah";
+            this.btnSiyah.Size = new System.Drawing.Size(118, 61);
+            this.btnSiyah.TabIndex = 2;
+            this.btnSiyah.Text = "Siyah ";
+            this.btnSiyah.UseVisualStyleBackColor = true;
+            this.btnSiyah.Click += new System.EventHandler(this.btnSiyah_Click);
+            // 
+            // tmrBeyaz
+            // 
+            this.tmrBeyaz.Interval = 1000;
+            this.tmrBeyaz.Tick += new System.EventHandler(this.tmrBeyaz_Tick);
+            // 
+            // tmrSiyah
+            // 
+            this.tmrSiyah.Interval = 1000;
+            this.tmrSiyah.Tick += new System.EventHandler(this.tmrSiyah_Tick);
+            // 
+            // btnBeyaz
+            // 
+            this.btnBeyaz.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBeyaz.Location = new System.Drawing.Point(181, 12);
+            this.btnBeyaz.Name = "btnBeyaz";
+            this.btnBeyaz.Size = new System.Drawing.Size(118, 61);
+            this.btnBeyaz.TabIndex = 1;
+            this.btnBeyaz.Text = "Beyaz ";
+            this.btnBeyaz.UseVisualStyleBackColor = true;
+            this.btnBeyaz.Click += new System.EventHandler(this.btnBeyaz_Click);
+            // 
+            // lblBeyaz
+            // 
+            this.lblBeyaz.AutoSize = true;
+            this.lblBeyaz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBeyaz.Location = new System.Drawing.Point(177, 76);
+            this.lblBeyaz.Name = "lblBeyaz";
+            this.lblBeyaz.Size = new System.Drawing.Size(58, 20);
+            this.lblBeyaz.TabIndex = 3;
+            this.lblBeyaz.Text = "Beyaz";
+            // 
+            // lblSiyah
+            // 
+            this.lblSiyah.AutoSize = true;
+            this.lblSiyah.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSiyah.Location = new System.Drawing.Point(177, 172);
+            this.lblSiyah.Name = "lblSiyah";
+            this.lblSiyah.Size = new System.Drawing.Size(53, 20);
+            this.lblSiyah.TabIndex = 4;
+            this.lblSiyah.Text = "Siyah";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(361, 237);
+            this.Controls.Add(this.lblSiyah);
+            this.Controls.Add(this.lblBeyaz);
+            this.Controls.Add(this.btnSiyah);
+            this.Controls.Add(this.btnBeyaz);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnSiyah;
+        private System.Windows.Forms.Timer tmrBeyaz;
+        private System.Windows.Forms.Timer tmrSiyah;
+        private System.Windows.Forms.Button btnBeyaz;
+        private System.Windows.Forms.Label lblBeyaz;
+        private System.Windows.Forms.Label lblSiyah;
+    }
+}
+
---
+++ satranc 815/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace satranc_815
+{
+    public partial class Form1 : Form
+    {
+        int sayacBeyaz = 10;
+        int sayacSiyah = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            tmrBeyaz.Start();
+        }
+
+
+
+        private void btnSiyah_Click(object sender, EventArgs e)
+        {
+            tmrSiyah.Stop();
+            tmrBeyaz.Start();
+        }
+
+        private void tmrBeyaz_Tick(object sender, EventArgs e)
+        {
+            sayacBeyaz--;
+            if (sayacBeyaz <= 0)
+            {
+                tmrBeyaz.Stop();
+                this.BackColor = Color.White;
+            }
+            lblBeyaz.Text = sayacBeyaz.ToString();
+            if (sayacBeyaz <= 0)
+            {
+                lblBeyaz.Text = "Siyah Kazandı";
+            }
+
+        }
+        private void btnBeyaz_Click(object sender, EventArgs e)
+        {
+            tmrBeyaz.Stop();
+            tmrSiyah.Start();
+
+           
+        }
+        private void tmrSiyah_Tick(object sender, EventArgs e)
+        {
+            sayacSiyah--;
+            if (sayacSiyah <= 0)
+            {
+                tmrBeyaz.Stop();
+                this.BackColor = Color.Black;
+            }
+            lblSiyah.Text = sayacSiyah.ToString();
+
+            if (sayacSiyah <= 0)
+            {
+                lblSiyah.Text = "Beyaz Kazandı";
+            }    
+            
+            
+
+        }
+
+       
+    }
+}
